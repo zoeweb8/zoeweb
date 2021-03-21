1a881c029871b828931310d32346dae11c035a7b
Zoe DeTrani <zd9575@rit.edu>
2021-03-20 22:47:56 -0400

    Updated the page layout, added design to home screen, added contact sheet

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import '.list.svg';
 import './App.css';
 import { Link } from 'react-scroll';
-import { FaGithub, FaLinkedin, FaCameraRetro, FaRunning,FaMountain,FaPaintBrush} from 'react-icons/fa';
-import { Fade, Bounce } from "react-awesome-reveal";
-import TypeWriterEffect from 'react-typewriter-effect';
+import { FaGithub, FaLinkedin, FaChevronCircleDown} from 'react-icons/fa';
+import { Fade, Bounce , Zoom} from "react-awesome-reveal";
+import ReactTypingEffect from 'react-typing-effect';
+import ContactForm from './ContactForm';
 
 function App() {
   return (
     <div>
+
       <ul class="navbar">
-        <div class="logo">Zoe DeTrani</div>
+          <Link to="home" smooth={false} class="logo">Zoe DeTrani</Link>
         <ul>
           <Link to="home" smooth={true} class="btn">Home</Link>
           <Link to="about" smooth={true} class="btn">About</Link>
@@ -18,61 +20,78 @@ function App() {
           <a class="mobileIcon" href="https://github.com/zotrani"><FaGithub /></a>
           <a class="mobileIcon" href="https://www.linkedin.com/in/ztrani"><FaLinkedin /></a>
         </ul>
-
       </ul>
-
-      <div class="home">
-
-        <TypeWriterEffect
-           textStyle={{
-             fontFamily: 'TypeWriter',
-             color: 'white',
-             fontWeight: 500,
-             fontSize: '50px',
-           }}
-           startDelay={500}
-           cursorColor="white"
-           multiText={[
-             'Hi, my name is Zoe.',
-             'Welcome to my page! :)',
-             'I currently study computer engineering, but I love coding as well!',
-             'Scroll to learn more about me!',
-             'Have a lovely day! <3',
-           ]}
-           loop={true}
-           nextTextDelay={1000}
-           typeSpeed={20}
-         />
+    <div class="home">
+      <div class="container">
+        <Zoom><img src="https://i.ibb.co/CHgTwsT/zoe-Overlay.png" height="auto" width="270px" max-width="50%"></img></Zoom >
       </div>
+      <div class="container">
+        <ReactTypingEffect
+          text={["'Hello! My name is Zoe. Welcome to my page!'",
+          "'I'm studying computer engineering at RIT.","'My favorite hobby is art (photography, drawing)!'","'Scroll down to learn more about me!'",
+          "'Have a lovely day! Thanks for visiting! <3'"]}
+          speed={70}
+          style={{
+           fontFamily: 'TypeWriter',
+           color: 'white',
+           fontWeight: 500,
+           fontSize: '20px',
+
+          }}
+          eraseSpeed={60}
+          eraseDelay={700}
+          staticText={["ZOE : "]}/>
+
+          <div class="arrow" >
+             <Link to="about" smooth={true} >  <a><FaChevronCircleDown /></a></Link>
+          </div>
+        </div>
+
+       </div>
+
+
       <div class="about">
-        <Bounce delay={200}>
+        <Bounce delay={100}>
               <p>About</p>
+              <img src="https://i.ibb.co/7Q3vdVC/zoe.jpg" alt="zoe" height="200px" width="200px" ></img>
         </Bounce>
-        <img src="https://i.ibb.co/7Q3vdVC/zoe.jpg" alt="zoe"></img>
         <a class="edu">
             <h1>Education </h1>
+               <Bounce delay={300}>
                 <li>Rochester Institute of Technology</li>
-                <li>Bachelors of Science: Computer Engineering</li>
+                <li>Bachelors of Science - Computer Engineering</li>
                 <li>Expected Graduation: May 2023</li>
+            </Bounce>
             <h1>Key Skills </h1>
-                <div class="bullets"><h2>Programming Languages:</h2> C, Java, Python 3.7, CSS, HTML, JavaScript, JSON, PowerShell </div>
-                <div class="bullets"><h2>Software Tools:</h2> GitHub, Git Bash, Linux, PyCharm, Visual Studio Code, IntelliJ, Atom, IDLE</div>
+                <Bounce delay={400}>
+                  <h2>Programming Skills:</h2>
+                    <li> C, Java, Python 3.7</li>
+                    <li> PowerShell, JSON </li>
+                    <li> HTML, JavaScript, CSS</li>
+                  <h2>Software Tools:</h2>
+                    <li>GitHub, Git Bash, Linux</li>
+                    <li>Visual Studio Code, IntelliJ</li>
+                    <li> Atom, PyCharm, IDLE</li>
+                </Bounce>
+                <div class="row">
+                  <a class="hobbyIcon" href="https://github.com/zotrani"><FaGithub /></a>
+                  <a class="hobbyIcon" href="https://www.linkedin.com/in/ztrani"><FaLinkedin /></a>
+                </div>
           </a>
-          <h3> Hobbies</h3>
-          <h3>
-            <li class="hobbyIcon"><FaCameraRetro /></li>
-            <li class="hobbyIcon"><FaRunning /></li>
-            <li class="hobbyIcon"><FaMountain /></li>
-            <li class="hobbyIcon"><FaPaintBrush /></li>
-            </h3>
-
-
       </div>
+
       <div class="blog">
         <h1>3</h1>
       </div>
       <div class="contact">
-        <h1>4</h1>
+        <p> Let's Be Friends!</p>
+          <div class="container">
+            <ContactForm/>
+          </div>
+          <footer>
+            Designed and Coded by Zoe with ♡ 2021
+        </footer>
+
       </div>
     </div>
 
